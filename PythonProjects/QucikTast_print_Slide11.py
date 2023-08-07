d35c9f9cb6c83e736479d91c4da2ee810a7c1ca9
Mohamed Samy <mohamedskamel@hotmail.com>
2023-08-07 23:17:29 +0300

    [PythonDev] dummy Task#1
    Notes:
    *This commit is only to test the correct set up of git extensions to github
    
    Added files :
    *QucikTast_print_Slide11.py :
    	- prints my name and some info with different shapes of print function.
    
    Added files pathes:
    /PythonProjects/QucikTast_print_Slide11.py
    
    Related Session: Python_session_1

---
+++ PythonProjects/QucikTast_print_Slide11.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+## method one
+print("Full Name: Mohamed Samy Ahmed\nBirth: 14/8/1999\nFaculty:GUC\nE-mail:mohamedsamykamel99@gmail.com")
+print("---------------------------------------------")
+## method two
+print("Full Name: Mohamed Samy Ahmed")
+print("Birth: 14/8/1999")
+print("Faculty:GUC")
+print("E-mail:mohamedsamykamel99@gmail.com")
+## method Three
+print("---------------------------------------------")
+print("Full Name: Mohamed Samy Ahmed" , end="\n")
+print("Birth: 14/8/1999" , end=",\n")
+print("Faculty:GUC" , end="\n")
+print("E-mail:mohamedsamykamel99@gmail.com")
