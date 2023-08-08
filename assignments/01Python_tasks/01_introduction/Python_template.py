c9bc7d43549cbfac6f81daf8277fcf36407f9ab1
Mohamed Samy <mohamedskamel@hotmail.com>
2023-08-08 23:40:03 +0300

    [PythonDev] Gitlens commit trial
    added files:
    *NONE
    
    modified files:
    *
    
    file paths:
    */assignments/01Python_tasks/01_introduction/Python_template.py
    */template/git_commit_msg_template.txt
    
    Related Session:
    *Python Sessions & overall repo management

---
+++ assignments/01Python_tasks/01_introduction/Python_template.py
@@ -0,0 +1,27 @@
+#########################################################################################################################
+#                                                                                        				#
+# File: example.py                                                                       				#
+# Author: Your Name                                                                      				#
+# Date: August 8, 2023                                                                   				#
+#                                                                                        				#
+# Description:                                                                           				#
+#    Provide a brief description of the purpose and functionality of the Python file.    				#
+#                                                                                        				#
+#########################################################################################################################
+#!/usr/bin/python3
+
+## method one
+print("Full Name: Mohamed Samy Ahmed\nBirth: 14/8/1999\nFaculty:GUC\nE-mail:mohamedsamykamel99@gmail.com")
+print("---------------------------------------------")
+## method two
+print("Full Name: Mohamed Samy Ahmed")
+print("Birth: 14/8/1999")
+print("Faculty:GUC")
+print("E-mail:mohamedsamykamel99@gmail.com")
+## method Three
+print("---------------------------------------------")
+print("Full Name: Mohamed Samy Ahmed" , end="\n")
+print("Birth: 14/8/1999" , end=",\n")
+print("Faculty:GUC" , end="\n")
+print("E-mail:mohamedsamykamel99@gmail.com")
+
