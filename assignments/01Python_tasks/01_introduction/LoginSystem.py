077211104cdccbbe3598975ca4d2fd676fa7debd
Mohamed Samy <mohamedskamel@hotmail.com>
2023-08-11 03:57:46 +0300

    [PythonSession1] Quick Task LoginSystem implementation
    added files:
    *LoginSystem.py
    *Commit_msg_template.txt
    
    modified files:
    *Adjusted the folder structure
    
    file paths:
    */assignments/01Python_tasks/01_introduction/Python_template.py
    */assignments/01Python_tasks/01_introduction/LoginSystem.py
    */template/git_commit_msg_template.txt
    
    Related Session:
    Python_Session1_introduction
    *Python Sessions & overall repo management

---
+++ assignments/01Python_tasks/01_introduction/LoginSystem.py
@@ -0,0 +1,31 @@
+#########################################################################################################################
+#                                                                                        			                	#
+# File: LoginSystem.py                                                                       			                #
+# Author: Mohamed Samy Ahmed                                                                     			            #
+# Date: August 11, 2023                                                                   			                	#
+#                                                                                        			                	#
+# Description:                                                                           			                	#
+#    Program that can handle simple login system with predefined credentials.           			                	#
+#                                                                                        			                	#
+#########################################################################################################################
+#!/usr/bin/python3
+
+# making directory contains the predefined credentials
+data_base = {
+    "Ahmed":"1394",
+    "Ali":"6078",
+    "Amr":"9345"
+}
+
+Username = input("Enter your Username :")
+Password = input("Enter your Password :")
+
+if Username in data_base.keys():
+    
+    if Password in data_base[Username]:
+        print("Welcome user")
+    else:
+        print("Invalid credentials")
+        
+else:
+    print("Invalid credentials")
